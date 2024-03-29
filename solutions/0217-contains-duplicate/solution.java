ee2d6c37c38849eb15ddb0124e7ba8c767ac4e1f
Sergey Gusev <serge.loie@yandex.ru>
2024-03-29 04:44:46 +0000

    [LeetCode Sync] Runtime - 7 ms (99.27%), Memory - 60.3 MB (11.56%)

---
+++ solutions/0217-contains-duplicate/solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            };
+        }
+        return set.size() != nums.length;
+    }
+}
