9152faa13741f1df699c3571fc438fa17389a069
Sergey Gusev <serge.loie@yandex.ru>
2024-07-03 12:46:51 +0000

    [LeetCode Sync] Runtime - 5 ms (1.28%), Memory - 42.1 MB (84.64%)

---
+++ solutions/2876-number-of-employees-who-met-the-target/solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+            return (int) Arrays.stream(hours)
+                .filter(x -> x >= target)
+                .count();
+    }
+}
