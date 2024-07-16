47c56ba67ecc1ce63b9a89a8d1095f237b057e0a
Sergey Gusev <serge.loie@yandex.ru>
2024-07-16 08:24:37 +0000

    [LeetCode Sync] Runtime - 46 ms (7.19%), Memory - 48.6 MB (16.98%)

--- solutions/0049-group-anagrams/solution.java
+++ solutions/0049-group-anagrams/solution.java
@@ -1,37 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-
-public class Solution {
+class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-        for (String str : strs) {
-            String anagramKey = sortWord(str);
-            map.compute(anagramKey, (key, value) -> {
-                if (value == null) {
-                    value = new ArrayList<>();
-                }
-                value.add(str);
-                return value;
-            });
-        }
-        List<List<String>> result = new ArrayList<>();
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
-            result.add(entry.getValue());
-        }
-        return result;
+        Map<String, List<String>> hashMap = Arrays.stream(strs)
+                .collect(Collectors.groupingBy(this::sortWord, Collectors.toList()));
+        return hashMap.values().stream().toList();
     }
 
-    public String sortWord(String str) {
-        return str.chars()
-                .mapToObj(c -> (char) c)
+    public String sortWord(String word) {
+        return Arrays.stream(word.split(""))
                 .sorted()
-                .map(String::valueOf)
-                .collect(Collectors.joining(""));
-    
+                .collect(Collectors.joining());
     }
+    
 }
