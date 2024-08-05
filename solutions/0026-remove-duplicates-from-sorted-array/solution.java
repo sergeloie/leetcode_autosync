22bb8f08326a71547d9d57edffd76beb0f199bb7
Sergey Gusev <serge.loie@yandex.ru>
2024-08-05 12:04:14 +0000

    [LeetCode Sync] Runtime - 1 ms (64.92%), Memory - 44.6 MB (84.18%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int l = 0;
-        int r = 0;
-        int end = nums.length - 1;
-
-        while (r < end) {
-            r++;
-            if (nums[l] < nums[r]) {
-                nums[l + 1] = nums[r];
-                l++;
+        int r = 1;
+        while (r < nums.length) {
+            while (nums[l] == nums[r]) {
+                r++;
+                if (r >= nums.length) {
+                    return l + 1;
+                }
             }
+            nums[++l] = nums[r];
         }
         return l + 1;
     }
